78ea93a531041ba9b28fa273c76a2c30e2df4d41
Javier Rodriguez <javicerodriguez@gmail.com>
2022-10-19 00:27:54 -0300

    🐛 fix: allow host from local run

--- tech_store_api/settings.py
+++ tech_store_api/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='SARASA')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
