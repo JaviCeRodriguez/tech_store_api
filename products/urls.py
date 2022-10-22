b80c6ac1cdda08c37e139f74375b8e1eaa0d5c65
Javier Rodriguez <javicerodriguez@gmail.com>
2022-10-22 00:10:04 -0300

    🐛 fix: routes

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ProductList, ProductDetail, CategoryList, CategoryDetail
+
+urlpatterns = [
+    path('products/', ProductList.as_view()),
+    path('products/<int:pk>/', ProductDetail.as_view()),
+    path('categories/', CategoryList.as_view()),
+    path('categories/<int:pk>/', CategoryDetail.as_view()),
+]
--- tech_store_api/urls.py
+++ tech_store_api/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from products import views as product_views
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -17,14 +16,10 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
-# TODO: Remove endpoints in this file and move them to products/urls.py
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
-    path('products/', product_views.ProductList.as_view()),
-    path('products/<int:pk>/', product_views.ProductDetail.as_view()),
-    path('categories/', product_views.CategoryList.as_view()),
-    path('categories/<int:pk>/', product_views.CategoryDetail.as_view()),
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('admin/', admin.site.urls),
+   path('', include('products.urls')),
 ]
